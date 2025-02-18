24d92b656f95d6875288dcb1edd7532e2804b9e5
Uvaancovie <uvaancovie@gmail.com>
2025-02-19 00:23:31 +0200

    dynamic import

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,16 @@
-"use client"
+// app/about/page.tsx
+import React from "react"
+import dynamic from "next/dynamic"
 
-import About from '@/components/About'
+const BrowserFeature = dynamic(() => import("@/components/BrowserFeature"), {
+  ssr: false,
+})
 
 export default function AboutPage() {
-  return <About />
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">About Page</h1>
+      <BrowserFeature />
+    </main>
+  )
 }
